772ca1f34e94d5a6e1eb56cd8b7bfdbab4d9546c
Aleš Trunda <alestrunda@gmail.com>
2020-09-27 09:02:21 +0200

    fix redis initialization

--- src/redis.js
+++ src/redis.js
@@ -1,21 +1,24 @@
 import redis from "redis";
 import { promisify } from "util";
 
-let client = {
-  set: () => {},
-};
+let client;
 let getAsync = () => undefined;
 
 const createClient = (args) => {
   const redisClient = redis.createClient(args);
   redisClient.on("error", (error) => {
+    client = createDummyClient();
     console.error(error);
   });
   redisClient.on("ready", () => {
-    getAsync = promisify(client.get).bind(client);
+    getAsync = promisify(redisClient.get).bind(redisClient);
     client = redisClient;
   });
 };
+
+const createDummyClient = () => ({
+  set: () => {},
+});
 
 const getClient = () => ({
   client,
